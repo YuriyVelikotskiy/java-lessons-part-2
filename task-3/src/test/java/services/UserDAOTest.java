1c8b16fb155cbe35578c07e8a4923f7b37352515
Юрий <urabaket98@gmail.com>
2025-11-13 04:56:41 +0500

    добавление модуля для задания по теме Unit Tests

---
+++ task-3/src/test/java/services/UserDAOTest.java
@@ -0,0 +1,28 @@
+package services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDAOTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ task-3/src/test/java/services/UserServiceTest.java
@@ -0,0 +1,28 @@
+package services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void findUser() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void findAllUsers() {
+    }
+}
